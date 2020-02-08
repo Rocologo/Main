88501e24048271c61fdf7e9cca5887b1f8d6d332
Rocologo <rocologo@hotmail.com>
2020-02-08 09:59:14 +0100

    Release 1.15.2

--- Core/src/one/lindegaard/Core/rewards/CoreCustomItems.java
+++ Core/src/one/lindegaard/Core/rewards/CoreCustomItems.java
@@ -13,6 +13,7 @@ import one.lindegaard.Core.v1_13_R1.Skins_1_13_R1;
 import one.lindegaard.Core.v1_13_R2.Skins_1_13_R2;
 import one.lindegaard.Core.v1_14_R1.Skins_1_14_R1;
 import one.lindegaard.Core.v1_15_R1.Skins_1_15_R1;
+//import one.lindegaard.Core.v1_15_R2.Skins_1_15_R2;
 import one.lindegaard.Core.v1_8_R1.Skins_1_8_R1;
 import one.lindegaard.Core.v1_8_R2.Skins_1_8_R2;
 import one.lindegaard.Core.v1_8_R3.Skins_1_8_R3;
@@ -44,7 +45,7 @@ public class CoreCustomItems {
 			return null;
 		}
 		// https://www.spigotmc.org/wiki/spigot-nms-and-minecraft-versions/
-		if (version.equals("v1_15_R2")) {
+		if (version.equals("v1_15_R1")) {
 			sk = new Skins_1_15_R1();
 		} else if (version.equals("v1_14_R1")) {
 			sk = new Skins_1_14_R1();
