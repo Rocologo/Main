7b85de46d4f58afd40199f758dc9998c1c9308f2
Rocologo <rocologo@hotmail.com>
2020-02-08 09:47:12 +0100

    Updated main to 1.15.2
    Core set to 0.2.1
    Shared set to 0.0.3

--- Core/src/one/lindegaard/Core/rewards/CoreCustomItems.java
+++ Core/src/one/lindegaard/Core/rewards/CoreCustomItems.java
@@ -12,6 +12,7 @@ import one.lindegaard.Core.v1_11_R1.Skins_1_11_R1;
 import one.lindegaard.Core.v1_13_R1.Skins_1_13_R1;
 import one.lindegaard.Core.v1_13_R2.Skins_1_13_R2;
 import one.lindegaard.Core.v1_14_R1.Skins_1_14_R1;
+import one.lindegaard.Core.v1_15_R1.Skins_1_15_R1;
 import one.lindegaard.Core.v1_8_R1.Skins_1_8_R1;
 import one.lindegaard.Core.v1_8_R2.Skins_1_8_R2;
 import one.lindegaard.Core.v1_8_R3.Skins_1_8_R3;
@@ -43,7 +44,9 @@ public class CoreCustomItems {
 			return null;
 		}
 		// https://www.spigotmc.org/wiki/spigot-nms-and-minecraft-versions/
-		if (version.equals("v1_14_R1")) {
+		if (version.equals("v1_15_R2")) {
+			sk = new Skins_1_15_R1();
+		} else if (version.equals("v1_14_R1")) {
 			sk = new Skins_1_14_R1();
 		} else if (version.equals("v1_13_R2")) {
 			sk = new Skins_1_13_R2();
