a77a994f51de6e1912baa434635183fecc14a978
Rocologo <rocologo@hotmail.com>
2021-06-29 23:23:28 +0200

    updating to MC1.17

--- Core/src/one/lindegaard/Core/rewards/CoreCustomItems.java
+++ Core/src/one/lindegaard/Core/rewards/CoreCustomItems.java
@@ -38,6 +38,7 @@ import one.lindegaard.Core.v1_14_R1.Skins_1_14_R1;
 import one.lindegaard.Core.v1_15_R1.Skins_1_15_R1;
 import one.lindegaard.Core.v1_16_R1.Skins_1_16_R1;
 import one.lindegaard.Core.v1_16_R2.Skins_1_16_R2;
+import one.lindegaard.Core.v1_17_R1.Skins_1_17_R1;
 import one.lindegaard.Core.v1_8_R1.Skins_1_8_R1;
 import one.lindegaard.Core.v1_8_R2.Skins_1_8_R2;
 import one.lindegaard.Core.v1_8_R3.Skins_1_8_R3;
@@ -72,7 +73,9 @@ public class CoreCustomItems {
 			return null;
 		}
 		// https://www.spigotmc.org/wiki/spigot-nms-and-minecraft-versions/
-		if (version.equals("v1_16_R2")) {
+		if (version.equals("v1_17_R1")) {
+			sk = new Skins_1_17_R1();
+		} else if (version.equals("v1_16_R2")) {
 			sk = new Skins_1_16_R2();
 		} else if (version.equals("v1_16_R1")) {
 			sk = new Skins_1_16_R1();
