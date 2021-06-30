5a0e8b8ba4e8d09ac0d5276e38715b1448725759
Rocologo <rocologo@hotmail.com>
2021-06-30 09:42:32 +0200

    Trying to make MC1.17 work

--- NMS_v1_16_R3/src/main/java/one/lindegaard/Core/v1_16_R3/Skins_1_16_R3.java
+++ NMS_v1_16_R3/src/main/java/one/lindegaard/Core/v1_16_R3/Skins_1_16_R3.java
@@ -1,12 +1,14 @@
 package one.lindegaard.Core.v1_16_R3;
 
-import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
+//import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 
-import net.minecraft.server.v1_16_R3.EntityPlayer;
+import net.minecraft.server.v1_16_R2.EntityPlayer;
+//import net.minecraft.server.v1_16_R3.EntityPlayer;
 import one.lindegaard.Core.shared.Skins;
 
 public class Skins_1_16_R3 implements Skins {
