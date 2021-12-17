f084cb4fa101f14835e2d7997b023cdf56a04216
Rocologo <rocologo@hotmail.com>
2021-12-17 22:58:26 +0100

    Mysql

--- Core/src/one/lindegaard/Core/storage/MySQLDataStore.java
+++ Core/src/one/lindegaard/Core/storage/MySQLDataStore.java
@@ -10,7 +10,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.Plugin;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 import one.lindegaard.Core.Core;
 
@@ -32,7 +33,7 @@ public class MySQLDataStore extends DatabaseDataStore {
 		try {
 			Locale.setDefault(new Locale("us", "US"));
 			Class.forName("com.mysql.jdbc.Driver");
-			MysqlDataSource dataSource = new MysqlDataSource();
+			MysqlDataSource dataSource = new MysqlConnectionPoolDataSource();
 			dataSource.setUser(Core.getConfigManager().databaseUsername);
 			dataSource.setPassword(Core.getConfigManager().databasePassword);
 			if (Core.getConfigManager().databaseHost.contains(":")) {
