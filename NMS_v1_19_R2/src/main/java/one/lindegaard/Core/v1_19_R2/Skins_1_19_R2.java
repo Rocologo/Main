dc3e3c7e1c64ddb3a7f2e1b12c9e096f261473b2
Rocologo <rocologo@rocologo.tk>
2023-04-10 15:27:52 +0200

    Build 0.1.4

---
+++ NMS_v1_19_R2/src/main/java/one/lindegaard/Core/v1_19_R2/Skins_1_19_R2.java
@@ -0,0 +1,29 @@
+package one.lindegaard.Core.v1_19_R2;
+
+import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+
+import net.minecraft.server.level.EntityPlayer;
+import one.lindegaard.Core.shared.Skins;
+
+public class Skins_1_19_R2 implements Skins {
+
+	// How to get Playerskin
+	// https://www.spigotmc.org/threads/how-to-get-a-players-texture.244966/
+
+	public String[] getSkin(Player player) {
+		EntityPlayer playerNMS = ((CraftPlayer) player).getHandle();
+		GameProfile profile = playerNMS.getBukkitEntity().getProfile();
+		String[] result = new String[2];
+		if (profile.getProperties().containsKey("textures")) {
+			Property property = profile.getProperties().get("textures").iterator().next();
+			result[0] = property.getValue();
+			result[1] = property.getValue();
+		}
+		return result;
+	}
+
+}
